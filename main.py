46df32638ade5dc2b337743db68b37e304989aba
Meridolian <mgueribout@gmail.com>
2020-11-04 10:53:30 +0100

    first breakpoint

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
 
 def print_hi(name, age):
-    print username
     username = input("what's your name ?")
     print("Hi " + username)
 
-    
-def exo3():
+
+def maximum():
     a = input("Enter a number")
     b = input("Enter a number")
-    number = max(a, b)
+    c = input("Enter a number")
+    number = max(a, b, c)
     print(number)
 
 
-def exo4() :
-    peer_or_odd = input("Enter a number")
-    if int(peer_or_odd) % 2 == 0:
+def peer_or_odd():
+    number = input("Enter a number")
+    if int(number) % 2 == 0:
         print("your number is peer")
     else:
         print("your number is odd")
 
 
+def major_or_minor():
+    age = int(input("How old are you"))
+    if age >= 18:
+        print("You are major")
+    else:
+        print("You are minor")
+
+
 if __name__ == '__main__':
-    print_hi('Meridolian', '22')
+    maximum()
 
